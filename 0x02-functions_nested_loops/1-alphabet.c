3a11bc281bfbbe4c30f2c5049ac15f2d851bd2a7
Asare_Dominic <asaredominic1@gmail.com>
2023-02-21 14:52:59 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+/**
+ * main - Entry point
+ * Description: prints alphabets to console.
+ * Return: Always 0 (success)
+ */
+
+#include <stdio.h>
+#include "main.h"
+int main(void)
+{
+void print_alphabet(void);
+
+
+return(0);
+}
