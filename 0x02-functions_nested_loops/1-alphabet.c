60bc321fcf64600c95e7c8512581252f56648873
Asare_Dominic <asaredominic1@gmail.com>
2023-02-21 17:02:34 -0800

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,8 +8,16 @@
 #include "main.h"
 int main(void)
 {
-void print_alphabet(void);
+void print_alphabet(void)
+{
+char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n')
+}
 
 
-return(0);
+return (0);
 }
