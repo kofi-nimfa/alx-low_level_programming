74d00238409dcb9da184994d8eed32b03f8a1da4
Asare_Dominic <asaredominic1@gmail.com>
2023-02-22 12:12:03 -0800

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,19 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * main - Entry point
- * Description: Prints message to console.
- * Return: Always 0 (success)
+ * main - print _putchar
+ * Return: 0
  */
 
 int main(void)
 {
-printf("_putchar\n");
-
-
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,23 +1,15 @@
+#include "main.h"
+
 /**
- * main - Entry point
- * Description: prints alphabets to console.
- * Return: Always 0 (success)
+ * print_alphabet - print all alphabet in lowercase
  */
 
-#include <stdio.h>
-#include "main.h"
-int main(void)
-{
 void print_alphabet(void)
 {
-char letter;
+	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
 
-	_putchar('\n')
-}
-
-
-return (0);
+	_putchar('\n');
 }
